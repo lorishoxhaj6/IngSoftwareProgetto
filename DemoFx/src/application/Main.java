14ab16d04710dd5980eca0c42b944b50a99803f4
Andrew-brgl <andrewbregoli@gmail.com>
2025-05-22 11:50:04 +0200

    prova se funziona la repository e inserimeto stage.setTitle()

--- DemoFx/src/application/Main.java
+++ DemoFx/src/application/Main.java
@@ -9,13 +9,15 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	//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
+	public void start(Stage stage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			stage.setScene(scene);
+			stage.setTitle("Glico");
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
