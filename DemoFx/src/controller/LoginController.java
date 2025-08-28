0dfb1d042210e4d8284cf721c58bb75f34cb9b58
Andrew-brgl <andrewbregoli@gmail.com>
2025-08-28 13:01:07 +0200

    nuovo

--- DemoFx/src/controller/LoginController.java
+++ DemoFx/src/controller/LoginController.java
@@ -47,7 +47,7 @@ public class LoginController implements Initializable {
 			try (ResultSet rs = ps.executeQuery()) {
 				if (rs.next() && rs.getInt(1) > 0) { // indica ho trovato un utente che ha username e pw corrispondenti
 					if (rbPatient.isSelected()) {
-						ViewNavigator.loadView("patientView.fxml");
+						ViewNavigator.loadView("prova.fxml");
 					} else {
 						if (rbDoctor.isSelected()) {
 							ViewNavigator.loadView("doctorView.fxml");
